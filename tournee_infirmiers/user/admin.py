701739a7602d066d9be8e1c1d024de1ce8b12925
Guillaume <guillaume.guerdoux@hotmail.fr>
2016-12-05 11:05:28 +0100

    create models person / nurse / patient / office

--- tournee_infirmiers/user/admin.py
+++ tournee_infirmiers/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from user.models import Person
+from user.models import Nurse
+from user.models import Patient
+from user.models import Office
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Nurse)
+admin.site.register(Patient)
+admin.site.register(Office)
--- tournee_infirmiers/user/models.py
+++ tournee_infirmiers/user/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Person(models.Model):
+	GENDER_CHOICES = (
+		('1', 'Homme',),
+		('2', 'Femme',),
+	)
+	PROFILE_TYPE_CHOICE = (
+		('NURSE', 'Nurse',),
+		('PATIENT', 'Patient',)
+	)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)  # One person is linked to one user
+	sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
+	address = models.CharField(max_length=255)
+	profile_type = models.CharField(max_length=255, choices=PROFILE_TYPE_CHOICE)
+	birthdate = models.DateField(null = True, blank=True)
+
+class Nurse(Person):
+	def __str__(self):
+		return "Nurse profile of {0}".format(self.user.username)
+
+class Patient(Person):
+	information = models.CharField(max_length=255)
+	def __str__(self):
+		return "Patient profile of {0}".format(self.user.username)
+
+class Office(models.Model):
+	address = models.CharField(max_length=255)
+	geographical_area = models.IntegerField(default = 30) # Geographical area where nurse can go / in Km
+	
