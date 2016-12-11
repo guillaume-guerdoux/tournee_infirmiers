aa3212c18d616a17b6e8f9de59d2d5ca099686fc
paulden <paul.de-nonancourt@student.ecp.fr>
2016-12-11 14:32:16 +0100

    Login required to display dashboard

--- tournee_infirmiers/home/views.py
+++ tournee_infirmiers/home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
 	return render(request, 'home/home.html')
 
+@login_required(redirect_field_name='dashboard')
 def dashboard(request):
 	return render(request, 'home/dashboard.html')
