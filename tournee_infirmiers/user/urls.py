fe7c3192633ce4e8c3389eae652cbd626991d9b4
paulden <paul.de-nonancourt@student.ecp.fr>
2016-12-11 13:20:07 +0100

    Authentication completed

--- tournee_infirmiers/user/urls.py
+++ tournee_infirmiers/user/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
-from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'user'
 urlpatterns = [
-    url(r'^connexion$', views.connexion, name='connexion'),
+    url(r'^login/$', auth_views.login, {'template_name': 'user/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
--- tournee_infirmiers/user/views.py
+++ tournee_infirmiers/user/views.py
@@ -1,23 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from .forms import ConnexionForm
 
-# Create your views here.
 
-def connexion(request):
-    error = False
 
-    if request.method == "POST":
-        form = ConnexionForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-            else:
-                error = True
-    else:
-        form = ConnexionForm()
-
-    return render(request, 'user/authentication.html', locals())
