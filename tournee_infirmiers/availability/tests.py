4734e2dff5348ef92ad456e50e66d1078be7cf5f
Guillaume <guillaume.guerdoux@student.ecp.fr>
2016-12-18 19:06:02 +0100

    tests for user application

--- tournee_infirmiers/availability/tests.py
+++ tournee_infirmiers/availability/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from user.models import Nurse
+from django.contrib.auth.models import User
 
+from datetime import date
+# FUNCTIONAL TESTS
+from user.function_test import UserCreationTests
 # Create your tests here.
---
+++ tournee_infirmiers/user/function_test.py
@@ -0,0 +1,17 @@
+# PARAMETER FOR TESTCASE 
+from django.test import TestCase, Client
+
+#MODELS
+from django.contrib.auth.models import User
+
+class UserCreationTests(TestCase):
+
+	def create_nurse(self, email, first_name, last_name, sex,
+		birthdate, address):
+		user = User.objects.create(username=email, first_name = first_name,
+			last_name = last_name)
+		user.save()
+		person = Nurse(user = user,
+			sex=sex, birthdate=birthdate, address=address, 
+			profile_type = "NURSE")
+		person.save()
