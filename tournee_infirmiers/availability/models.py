1a940096ac52d1a4c520ce552fa4e01f48c3a2a2
Guillaume <guillaume.guerdoux@student.ecp.fr>
2016-12-18 18:44:01 +0100

    modify home.html

--- tournee_infirmiers/availability/models.py
+++ tournee_infirmiers/availability/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from user.models import Nurse
 # Create your models here.
+class AvailabilityGroup(models.Model):
+	def __str__(self):
+		return "Group"
+		
+class Availability(models.Model):
+	nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+	start_date = models.DateTimeField()
+	duration = models.DurationField()
+	availability_group = models.ForeignKey(AvailabilityGroup, on_delete=models.CASCADE)
+
+
