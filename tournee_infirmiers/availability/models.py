e2c0fa0559d3006abe94b10cde285109338894bf
Guillaume <guillaume.guerdoux@student.ecp.fr>
2016-12-18 19:37:24 +0100

    nurse field in availability group and add a frequency field

--- tournee_infirmiers/availability/models.py
+++ tournee_infirmiers/availability/models.py
@@ -3,11 +3,17 @@ from django.db import models
 from user.models import Nurse
 # Create your models here.
 class AvailabilityGroup(models.Model):
+	FREQUENCY = (
+		('U', 'Unique',),
+		('D', 'Tous les jours',),
+		('W', 'Toutes les semaines',),
+	)
+	nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+	frequency = models.CharField(max_length=1, choices=FREQUENCY)
 	def __str__(self):
 		return "Group"
 		
 class Availability(models.Model):
-	nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE)
 	start_date = models.DateTimeField()
 	duration = models.DurationField()
 	availability_group = models.ForeignKey(AvailabilityGroup, on_delete=models.CASCADE)
