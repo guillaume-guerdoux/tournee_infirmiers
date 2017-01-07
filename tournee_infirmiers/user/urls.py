b300729827ede8fe29e0233582d6c8aeff4df666
paulden <paul.de-nonancourt@student.ecp.fr>
2017-01-07 12:47:43 +0100

    Added login page for nurse offices

--- tournee_infirmiers/user/urls.py
+++ tournee_infirmiers/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^error_login/', user_views.error_login),
     url(r'^patient/$', user_views.patient_info),
     url(r'^login/$', auth_views.login, {'template_name': 'user/login.html'}, name='login'),
+    url(r'^office_login/$', auth_views.login, {'template_name': 'user/office_login.html'}, name='office_login'),
     url(r'^logged_out/$', auth_views.logout, {'template_name': 'user/logged_out.html'}, name='logout'),
 ]
