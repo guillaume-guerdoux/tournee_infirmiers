a72dfce835e9a139352be3db07ee762305a71191
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-01-08 16:54:32 +0100

    Add database retrieval

--- tournee_infirmiers/patient/views.py
+++ tournee_infirmiers/patient/views.py
@@ -24,38 +24,45 @@ def patient(request):
     return render(request, 'patient/new_patient.html', locals())
 
 
-def patient_info(request):
+def patient_info(request, id_patient):
     # Create a patient object to be displayed in the template.
     # Need to fetch in from the database once we have a way of creating it.
-    patient = models.Patient()
-    patient.user = User()
-    patient.user.first_name = 'Pierre'
-    patient.user.last_name = 'Durand'
-    patient.user.email = 'pdurand@domainname.com'
-    patient.sex = '1'
-    patient.address = '2 avenue Sully Prudhomme, 92295 Châtenay-Malabry'
-    patient.profile_type = 'PATIENT'
-    patient.birthdate = date(1985, 8, 23)
-    patient.information = "Il est gentil."
+    try:
+        patient = models.Patient.objects.get(user_id=id_patient)
+    except models.Patient.DoesNotExist:
+        # Pour l'instant si le patient n'existe pas on en crée un fictif pour tester la vue
+        patient = models.Patient()
+        patient.user = User()
+        patient.user.first_name = 'Pierre'
+        patient.user.last_name = 'Durand'
+        patient.user.email = 'pdurand@domainname.com'
+        patient.sex = '1'
+        patient.address = '2 avenue Sully Prudhomme, 92295 Châtenay-Malabry'
+        patient.profile_type = 'PATIENT'
+        patient.birthdate = date(1985, 8, 23)
+        patient.information = "Il est gentil."
 
     return render(request, 'patient/patient_info.html', {'patient': patient})
 
 
 def patient_list(request):
     # retrieves the list of patients in the database
-    patients = []
+    patients_list = list(models.Patient.objects.all())
 
-    patient = models.Patient()
-    patient.user = User()
-    patient.user.first_name = 'Pierre'
-    patient.user.last_name = 'Durand'
-    patient.user.email = 'pdurand@domainname.com'
-    patient.sex = '1'
-    patient.address = '2 avenue Sully Prudhomme, 92295 Châtenay-Malabry'
-    patient.profile_type = 'PATIENT'
-    patient.birthdate = date(1985, 8, 23)
-    patient.information = "Il est gentil."
+    if len(patients_list) == 0:
+        # Temporary patient to fill up the list until we create patients in database
+        patient = models.Patient()
+        patient.user = User()
+        patient.user.first_name = 'Pierre'
+        patient.user.last_name = 'Durand'
+        patient.user.email = 'pdurand@domainname.com'
+        patient.sex = '1'
+        patient.address = '2 avenue Sully Prudhomme, 92295 Châtenay-Malabry'
+        patient.profile_type = 'PATIENT'
+        patient.birthdate = date(1985, 8, 23)
+        patient.information = "Il est gentil."
+        patient.id = 0
 
-    patients.append(patient)
+        patients_list.append(patient)
 
-    return render(request, 'patient/patient_list.html', {'patients': patients})
+    return render(request, 'patient/patient_list.html', {'patients': patients_list})
