5d0fd1475c95075a4fc755fd14815a4cb8688886
paulden <paul.de-nonancourt@student.ecp.fr>
2017-01-16 11:12:07 +0100

    Implemented registration form

--- tournee_infirmiers/user/views.py
+++ tournee_infirmiers/user/views.py
@@ -1,6 +1,11 @@
+from .forms import *
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 
 def error_login(request):
@@ -10,3 +15,22 @@ def error_login(request):
 @login_required(redirect_field_name='account')
 def account(request):
     return render(request, 'user/account.html', {'user': request.user})
+
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+            return HttpResponseRedirect('success/')
+    form = RegistrationForm()
+    variables = RequestContext(request, {'form': form})
+    return render_to_response('user/register.html', variables)
+
+
+def register_success(request):
+    return render_to_response(
+        'user/success.html',
+        context=RequestContext(request)
+    )
