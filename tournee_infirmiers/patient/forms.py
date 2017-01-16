41302713f20a0f33c62b4f89f00407c4ce3e2432
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-01-16 10:25:04 +0100

    Add correct form validation

--- tournee_infirmiers/patient/forms.py
+++ tournee_infirmiers/patient/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.validators import RegexValidator
+
 
 class PatientForm(forms.Form):
     GENDER_CHOICES = (
@@ -6,13 +8,16 @@ class PatientForm(forms.Form):
         ('2', 'Femme',)
     )
 
-    sex = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=GENDER_CHOICES, label="Genre")
+    sex = forms.ChoiceField(widget=forms.RadioSelect, choices=GENDER_CHOICES, label="Genre")
     lastname = forms.CharField(max_length=255, label="Nom")
     firstname = forms.CharField(max_length=255, label="Prénom")
     birthdate = forms.DateField(widget=forms.DateInput, label="Date de naissance")
     address = forms.CharField(max_length=255, label="Adresse")
-    postcode = forms.IntegerField(label="Code Postal")
+    postcode = forms.CharField(label="Code Postal",
+                               validators=[RegexValidator(r'^[0-9]{5}$', 'Entrez un code postal valide')])
     city = forms.CharField(max_length=255, label="Ville")
-    email = forms.CharField(max_length=255, label="Email")
-    phone = forms.CharField(max_length=255, label="Numéro de téléphone")
-    comments = forms.CharField(widget=forms.Textarea, label="Remarques")
+    email = forms.CharField(widget=forms.EmailInput, max_length=255, label="Email")
+    phone = forms.CharField(max_length=255, label="Numéro de téléphone",
+                            validators=[RegexValidator(r'^0[0-9]([ .-]?[0-9]{2}){4}$',
+                                                       'Entrez un numéro de téléphone valide (et commençant par 0).')])
+    comments = forms.CharField(widget=forms.Textarea, label="Remarques", required=False)
