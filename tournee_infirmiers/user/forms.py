7b1a92394e24da896422b794c8d7b82aa9a7c2c8
paulden <paul.de-nonancourt@student.ecp.fr>
2017-01-16 11:21:45 +0100

    Linked registration form to home page

--- tournee_infirmiers/user/forms.py
+++ tournee_infirmiers/user/forms.py
@@ -6,11 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=30)
+    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
     email = forms.EmailField(label='Email')
-    password1 = forms.CharField(label='Password',
+    password1 = forms.CharField(label='Mot de passe',
                           widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Password (Again)',
+    password2 = forms.CharField(label='Mot de passe (vérification)',
                         widget=forms.PasswordInput())
 
     def clean_password2(self):
@@ -19,14 +19,14 @@ class RegistrationForm(forms.Form):
             password2 = self.cleaned_data['password2']
             if password1 == password2:
                 return password2
-        raise forms.ValidationError('Passwords do not match.')
+        raise forms.ValidationError('Les mots de passe sont différents')
 
     def clean_username(self):
         username = self.cleaned_data['username']
         if not re.search(r'^\w+$', username):
-            raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
+            raise forms.ValidationError("Le nom d'utilisateur ne peut contenir que des caractères alphanumériques et underscore")
         try:
             User.objects.get(username=username)
         except ObjectDoesNotExist:
             return username
-        raise forms.ValidationError('Username is already taken.')
+        raise forms.ValidationError("Le nom d'utilisateur est déjà pris.")
