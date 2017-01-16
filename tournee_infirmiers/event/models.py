5c32fbd46b37aba64586f7fb1ebea77755cd873d
Locqueville Grégoire <gregoire.locqueville@student.ecp.fr>
2017-01-16 12:33:51 +0100

    Improved add_need page

--- tournee_infirmiers/event/models.py
+++ tournee_infirmiers/event/models.py
@@ -3,6 +3,7 @@ from patient.models import Patient
 
 
 class Need(models.Model):
+    need_string = models.CharField(max_length=4)
     start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     duration = models.DurationField()
     patient = models.ForeignKey('patient.Patient', on_delete=models.CASCADE)
