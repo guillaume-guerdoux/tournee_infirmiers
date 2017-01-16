279e3e8f77d58440764c4ff659ab0ebe62aada94
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-01-16 11:58:53 +0100

    Add patient edition possibility

--- tournee_infirmiers/patient/urls.py
+++ tournee_infirmiers/patient/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'patient/new/$', views.new_patient, name="new_patient"),
     url(r'patient/(\d+)$', views.patient_info, name="patient_info"),
     url(r'patient/list/$', views.patient_list, name="patient_list"),
-    url(r'patient/delete/(\d+)$', views.delete_patient, name="delete_patient"),
+    url(r'patient/(\d+)/delete/$', views.delete_patient, name="delete_patient"),
+    url(r'patient/(\d+)/edit/$', views.edit_patient, name="edit_patient"),
 ]
