122f5746719bbc368900dc4ff13f5c5f9a71271c
Paul de Nonancourt <paul.de-nonancourt@student.ecp.fr>
2017-01-22 22:14:58 +0100

    Added forms for needs

--- tournee_infirmiers/event/forms.py
+++ tournee_infirmiers/event/forms.py
@@ -4,6 +4,9 @@ from .models import Need
 
 
 class AddNeedForm(forms.Form):
-    need_string = forms.CharField(min_length=3, max_length=4, label='Indice du traitement')
-    start_time = forms.DateTimeField(label='Date et heure de début')
-    duration = forms.DurationField(label='Durée')
+    need_string = forms.CharField(min_length=3, max_length=4, label='Indice du traitement',
+                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Indice du traitement'}))
+    start_time = forms.DateTimeField(label='Date et heure de début',
+                                     widget=forms.DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Date et heure de début'}))
+    duration = forms.DurationField(label='Durée',
+                                   widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Durée'}))
