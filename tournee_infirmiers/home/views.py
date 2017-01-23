a0dedd05268b8185efc07d063ecddd2a2eb441c4
Grégoire Locqueville <gregoire.locqueville@student.ecp.fr>
2017-01-23 11:07:52 +0100

    Redirect home to dashboard

--- tournee_infirmiers/home/views.py
+++ tournee_infirmiers/home/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def home(request):
-	return render(request, 'home/home.html')
+    if request.user.is_authenticated():
+        return redirect('dashboard/')
+    else:
+        return render(request, 'home/home.html')
 
 
 @login_required(redirect_field_name='dashboard')
