206601251f8cbef2ac9a7e9ce936c944bb2a435a
paulden <paul.de-nonancourt@student.ecp.fr>
2017-01-23 09:46:22 +0100

    Changed success location in event/views

--- tournee_infirmiers/event/views.py
+++ tournee_infirmiers/event/views.py
@@ -8,7 +8,7 @@ def add_need(request, patient_number):
     form = AddNeedForm(request.POST or None)
     current_patient = Patient.objects.get(id=patient_number)
     if form.is_valid():
-        success = True
         need = Need(start_time=form.cleaned_data['start_time'], duration=form.cleaned_data['duration'], patient=current_patient)
         need.save()
+        success = True
     return render(request, 'need/add_need.html', locals())
