1700a6071481d194e8079ec577b9d9a118cafc1e
Amaury DUVAL <contact@amauryduval.com>
2017-01-23 11:39:01 +0100

    Integrated optimisation algorithm

--- tournee_infirmiers/optimizer/views.py
+++ tournee_infirmiers/optimizer/views.py
@@ -6,37 +6,75 @@ from user.models import Nurse
 from django.http import JsonResponse
 import json
 from django.core import serializers
-from datetime import time, datetime
+from datetime import time, datetime, timedelta
+from optimizer.models import EvolutionaryOptimizer
+import numpy as np
 
 # Create your views here.
 
 def optimize(request, year, month, day):
-	''' 
-		K : nb_nurses
-		P : 
-		T : 
-		D : 
-		N : workday
-		h : startday
-		mandatory_schedule : mandatory_schedule 
-	'''
-	# year, month, day = 2017, 1, 17
+	generate_schedule_file(year, month, day)
+	return HttpResponse("Done")
 
+def get_schedule_file_path(year, month, day):
+	return 'optimizer/schedules/{}_{}_{}'.format(year, month, day)
+
+def generate_schedule_file(year, month, day):
+	data = get_data_from_db(year, month, day)
+
+	heal_duration_vector = [timedelta(minutes=30) for i in range(data['nb_needs'])]
+	time_distance_matrix = get_time_distance_matrix_from_adresses(data['addresses'])
+
+	identity = np.identity(data['nb_needs'])
+	ones = np.ones(data['nb_needs'])
+	time_distance_matrix = ones - identity
+
+
+	try:
+		evolutionary_optimizer = EvolutionaryOptimizer(
+										nurse_nb = data['nb_nurses'],
+										heal_nb = data['nb_needs'],
+										time_distance_matrix = time_distance_matrix,
+										heal_duration_vector = heal_duration_vector,
+										mandatory_schedules = data['mandatory_schedules'])
+		optimize_output = evolutionary_optimizer.get_optimize_population()
+
+		schedules = []
+		for i in range(len(optimize_output)):
+			schedule = {
+				'nurse_id': data['nurses'][i].id,
+				'ordered_need_ids': [data['needs'][n].id for n in optimize_output[i]]
+			}
+			schedules.append(schedule)
+	except: 
+		print("Error")
+		schedules = [{'error': True, 'nurse_id': data['nurses'][i].id, 'ordered_need_ids': []} for i in range(data['nb_nurses'])]
+
+
+	with open(get_schedule_file_path(year, month, day), 'w') as outfile:
+	    json.dump(schedules, outfile)
+
+
+def get_data_from_db(year, month, day): 
 	data = {
 		'date': datetime(int(year), int(month), int(day)),
 		'workday': 8 * 60 * 60,
 		'startday': time(8),
 		'addresses': {},
-		'durations': {},
-		'mandatory_schedules': {}
+		'nurses': {},
+		# 'durations': {},
+		'mandatory_schedules': {},
+		'needs': {},
 	}
 
 	needs = Need.objects.all().filter(start_time__year=year,
-					                  start_time__month=month, 
-                         			  start_time__day=day)
+									  start_time__month=month, 
+									  start_time__day=day)
 	nurses = Nurse.objects.all()
 
 	data['nb_nurses'] = len(nurses)
+	for i in range(len(nurses)): 
+		data['nurses'][i] = nurses[i]
 	data['nb_patients'] = len(set([need.patient for need in needs]))
 	data['nb_needs'] = len(needs)
 
@@ -44,11 +82,12 @@ def optimize(request, year, month, day):
 	for i in range(len(needs)):
 		need = needs[i]
 		data['addresses'][i] = "{} {} {}".format(need.patient.address, need.patient.postcode, need.patient.city)
-		data['durations'][i] = need.duration
+		# data['durations'][i] = need.duration
 		data['mandatory_schedules'][i] = (need.start_time.time(), (need.start_time + need.duration).time()) if need.start_time.time() != time() else None
+		data['needs'][i] = need
+
+	return data
 
 
-	print(data)
-	
-	# data = serializers.serialize('json', needs)
-	return HttpResponse(data, content_type="application/json")
+def get_time_distance_matrix_from_adresses(addresses):
+	pass
