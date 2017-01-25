2d4ce39dc021525f18837cd229a47b4fa33f8fbc
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-01-25 09:37:22 +0100

    Add appointment detail page

--- tournee_infirmiers/event/views.py
+++ tournee_infirmiers/event/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from patient.models import Patient
-from .models import Need
+from .models import Need, Appointment
 from .forms import AddNeedForm
 
 
@@ -16,3 +16,13 @@ def add_need(request, patient_number):
         need.save()
         success = True
     return render(request, 'need/add_need.html', locals())
+
+
+def appointment_detail(request, id_appointment):
+    try:
+        appointment = Appointment.objects.get(id=id_appointment)
+        needs = appointment.need_set.all()
+        patient = needs[0].patient
+    except Appointment.DoesNotExist:
+        exception_raised = True
+    return render(request, 'event/appointment_detail.html', locals())
--- tournee_infirmiers/home/views.py
+++ tournee_infirmiers/home/views.py
@@ -4,27 +4,28 @@ import optimizer.views as opt
 from datetime import datetime
 import os.path
 
+
 # Create your views here.
 def home(request):
-	if request.user.is_authenticated():
-		return redirect('dashboard/')
-	else:
-		return render(request, 'home/home.html')
+    if request.user.is_authenticated():
+        return redirect('dashboard/')
+    else:
+        return render(request, 'home/home.html')
 
 
 @login_required(redirect_field_name='dashboard')
 def dashboard(request, year=None, month=None, day=None, user_id=None):
-	now = datetime.now()
-	year = year if year is not None else now.year
-	month = month if month is not None else now.month
-	day = day if day is not None else now.day
-	
-	try:
-		nurse_id = int(user_id) if user_id is not None else request.user.nurse.id
-		schedule = opt.get_schedule_for_nurse(nurse_id, year, month, day)
-	except Exception as e: 
-	  print("EXCEPTION : {}".format(e))
-	  schedule = []
+    now = datetime.now()
+    year = year if year is not None else now.year
+    month = month if month is not None else now.month
+    day = day if day is not None else now.day
 
-	# print(year, month, day, nurse_id, schedule)
-	return render(request, 'home/dashboard.html')
+    try:
+        nurse_id = int(user_id) if user_id is not None else request.user.nurse.id
+        schedule = opt.get_schedule_for_nurse(nurse_id, year, month, day)
+    except Exception as e:
+        print("EXCEPTION : {}".format(e))
+        schedule = []
+
+    # print(year, month, day, nurse_id, schedule)
+    return render(request, 'home/dashboard.html')
