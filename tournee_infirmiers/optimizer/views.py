7c6c1793b3aa1ff10b93439d419652f4f397df84
alicelasseigne <alice.lasseigne@student.ecp.fr>
2017-01-25 10:38:22 +0100

    matrices de distances

--- tournee_infirmiers/optimizer/views.py
+++ tournee_infirmiers/optimizer/views.py
@@ -10,6 +10,7 @@ from datetime import time, datetime, timedelta
 from optimizer.models import EvolutionaryOptimizer
 import numpy as np
 import os
+import pickle
 
 # Create your views here.
 
@@ -110,4 +111,45 @@ def get_schedule_for_nurse(nurse_id, year, month, day):
 
 
 def get_time_distance_matrix_from_adresses(addresses):
-	pass
+    with open('all_addresses_matrix', 'rb') as test:
+        depickler_test = pickle.Unpickler(test)
+        all_addresses_matrix = depickler_test.load()
+
+    all_addresses = ['140 avenue jean jaurès 92290 Chatenay Malabry',
+                     '14 rue du Docteur le Savoureux 92290 Chatenay Malabry',
+                     '22 rue de Saclay 92290 Chatenay Malabry', '31 rue Yvonne 92330 Bourg-la-Reine',
+                     '147 rue de Chalais 94240 L\'Hay-les-Roses', '6 rue de l\'Abbaye 92160 Antony',
+                     '120 avenue François Molé 92160 Antony', '44 rue Maurice Ténine 94260 Fresnes',
+                     '15 avenue du 8 mai 1945 94260 Fresnes', '10 avenue de la Gare 92330 Sceaux',
+                     '4 boulevard du marechal juin 91370 Verrières-le-buisson',
+                     '19 allée georges brassens 92290 Chatenay-Malabry', '93 rue du colonel fabien 92160 Antony',
+                     '3 rue léo delibes 92330 Sceaux', '20 rue de l\'eglise 91370 verrièes-le-buisson',
+                     '29 place des ailantes 92330 Sceaux', '67 boulevard pasteur 94260 Fresnes',
+                     '16 rue Eistein 92160 Antony', '5 avenue Sully Prud\'homme 92290 Chatenay Malabry',
+                     '18 rue Achille Garnon 92330 Sceaux', '36 rue des grands chênes 91370 Verrières-le-buisson',
+                     '4 rue prosper legouté 92160 Antony' '212 avenue du président kennedy 92160 antony',
+                     '10 rue boucicaut 92260 Fontenay-aux-roses', '55 rue vincent fayo 92290 Chatenay Malabry',
+                     '8 rue jean louis sinet 92330 Sceaux', '21 rue des blagis 92340 Bourg-la-reine',
+                     '63 rue de l\'yser 92330 Sceaux', '1 rue le bouvier 92340 Bourg-la-reine',
+                     '31 rue delabergerie 92340 Bourg-la-reine', "27 rue de la chrétienté 92330 Sceaux"]
+
+    nb_addresses = len(addresses)
+    matrix = []
+    for i in range(nb_addresses):
+        index_i = all_addresses.index(addresses[i])
+        line = []
+        for j in range(nb_addresses):
+            index_j = all_addresses.index(addresses[j])
+            line.append(all_addresses_matrix[index_i][index_j])
+        matrix.append(line)
+
+    return matrix
+
+
+test_addresses = ['140 avenue jean jaurès 92290 Chatenay Malabry',
+                  '14 rue du Docteur le Savoureux 92290 Chatenay Malabry', '22 rue de Saclay 92290 Chatenay Malabry',
+                  '120 avenue François Molé 92160 Antony', '44 rue Maurice Ténine 94260 Fresnes',
+                  '15 avenue du 8 mai 1945 94260 Fresnes', '10 avenue de la Gare 92330 Sceaux',
+                  '4 boulevard du marechal juin 91370 Verrières-le-buisson']
+
+print(get_time_distance_matrix_from_adresses(test_addresses))
