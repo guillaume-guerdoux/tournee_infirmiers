0d4f1a3f67df6097416f9d4206b683a64d05d673
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-02-01 17:21:37 +0100

    Fix availability urls

--- tournee_infirmiers/availability/urls.py
+++ tournee_infirmiers/availability/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'availability'
 urlpatterns = [
-    url(r'^dashboard/manage_availability$', views.manage_availabilities, name='manage_availability'),
-    url(r'^dashboard/create_availability/(\d+)$', views.add_availability, name='add_availability'),
-    url(r'^dashboard/choose_nurse$', views.choose_nurse, name='nurse_choice'),
+    url(r'^availability/list$', views.manage_availabilities, name='manage_availability'),
+    url(r'^availability/new/(\d+)$', views.add_availability, name='add_availability'),
+    url(r'^availability/choose_nurse$', views.choose_nurse, name='nurse_choice'),
     url(r'^ajax/remove_unique_availability$', views.remove_unique_availability, name='remove_unique_availability'),
     url(r'^ajax/remove_repeatly_availability_only_this_one$',
         views.remove_repeatly_availability_only_this_one, name='remove_repeatly_availability_only_this_one'),
